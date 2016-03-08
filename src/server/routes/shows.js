8b50e85ab5d8f08b50504dc3fd598819179ecc90
rlgaines <robertlg35@gmail.com>
2016-03-08 11:24:53 -0700

    added get routes

--- src/server/routes/shows.js
+++ src/server/routes/shows.js
@@ -6,8 +6,33 @@ function Shows() {
     return knex('shows');
 };
 
+// router.get('/shows', function(req, res, next) {
+//   res.status(200).json('testing');
+// });
+
 router.get('/shows', function(req, res, next) {
-  res.status(200).json('testing');
+  // res.status(200).json('testing');
+  Shows().select().then(function(result){
+    res.status(200).json(result);
+  });
 });
 
+
+//single route
+router.get('/show/:id', function(req, res, next) {
+  // res.status(200).json('testing');
+  Shows().select(req.body.id).then(function(result){
+    res.status(200).json(result);
+  });
+});
+
+//post request
+router.post('/shows', function(req, res, next){
+	// console.log(req.body)
+	// console.log(res.body)
+	Shows().insert(req.body).then(function(result){
+    res.status(200).json(result);
+  });
+	console.log(res.body)
+})
 module.exports = router;
--- test/apiTest.js
+++ test/apiTest.js
@@ -49,8 +49,66 @@ describe('API routes', function() {
                 done();
             });
         });
-
     });
 
+    describe('Get one show', function() {
+
+        it('should get one show', function(done) {
+            chai.request(server)
+            .get('/api/show/3')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.equal(4);
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.equal('Suits');
+                res.body[0].should.have.property('channel');
+                res.body[0].channel.should.equal('USA Network');
+                res.body[0].should.have.property('genre');
+                res.body[0].genre.should.equal('Drama');
+                res.body[0].should.have.property('rating');
+                res.body[0].rating.should.equal(3);
+                res.body[0].should.have.property('explicit');
+                res.body[0].explicit.should.equal(false);
+                done();
+            });
+        });
+    });
+
+
+    it('should POST a show', function(done) {
+    chai.request(server)
+    .post('/api/shows')
+    .send({
+        name: 'new show',
+        channel : 'ABC',
+        genre: 'Anything',
+        rating: 1,
+        explicit: false
+    })
+        .end(function(err, res) {
+
+            chai.request(server)
+            .get('/api/show/' + res.body[0])
+            .end(function(error, response) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.equal(4);
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.equal('new show');
+                res.body[0].should.have.property('channel');
+                res.body[0].channel.should.equal('ABC');
+                res.body[0].should.have.property('genre');
+                res.body[0].genre.should.equal('Anything');
+                res.body[0].should.have.property('rating');
+                res.body[0].rating.should.equal(1);
+                res.body[0].should.have.property('explicit');
+                res.body[0].explicit.should.equal(false);
+                done()
+               })
+        });
+    });
 });
 
